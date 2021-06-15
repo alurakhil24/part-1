fca9deb605b21abb0ad6905bbee3987383dc7fd0
Alur Akhil <alur.akhil@gmail.com>
2021-06-15 10:31:57 +0530

    Removed the comment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./views/home";
 import About from "./views/about";
 import SignIn from "./views/signin";
